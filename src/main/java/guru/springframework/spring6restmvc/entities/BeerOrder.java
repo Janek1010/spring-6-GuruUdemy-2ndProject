6b5c6a03184870abf9e09bd741f9840b87d88d26
Janek1010 <janekdomozych99@gmail.com>
2023-10-03 08:53:33 +0200

    mapping relations

--- src/main/java/guru/springframework/spring6restmvc/entities/BeerOrder.java
+++ src/main/java/guru/springframework/spring6restmvc/entities/BeerOrder.java
@@ -25,6 +25,8 @@ import org.hibernate.annotations.UpdateTimestamp;
 import org.hibernate.type.SqlTypes;
 
 import java.sql.Timestamp;
+import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 @Getter
@@ -64,4 +66,6 @@ public class BeerOrder {
     @ManyToOne
     private Customer customer;
 
+    @OneToMany(mappedBy = "beerOrder")
+    private Set<BeerOrderLine> beerOrderLines;
 }
