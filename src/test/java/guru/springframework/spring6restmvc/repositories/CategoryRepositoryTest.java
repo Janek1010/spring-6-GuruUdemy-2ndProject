0da12048cd05f53aa3dfdbc25550d847ee351004
Janek1010 <janekdomozych99@gmail.com>
2023-10-05 22:43:59 +0200

    CAtegoryRepo

---
+++ src/test/java/guru/springframework/spring6restmvc/repositories/CategoryRepositoryTest.java
@@ -0,0 +1,41 @@
+package guru.springframework.spring6restmvc.repositories;
+
+import guru.springframework.spring6restmvc.entities.Beer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class CategoryRepositoryTest {
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    @Autowired
+    BeerRepository beerRepository;
+
+    Beer testBeer;
+
+    @BeforeEach
+    void setUp() {
+        testBeer = beerRepository.findAll().get(0);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
